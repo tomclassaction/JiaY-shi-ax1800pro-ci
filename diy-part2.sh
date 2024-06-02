08316e15f7df827c1673f72464b5e03f5b539476
tom class <12421167+tomclassplus@users.noreply.github.com>
2024-06-02 11:28:24 +0800

    Update diy-part2.sh add wifi

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,44 @@ sed -i 's/192.168.1.1/192.168.123.23/g' package/base-files/files/bin/config_gene
 
 # 默认开启 wifi
 # sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+
+mkdir -p files/etc/config
+
+
+tee files/etc/config/wireless <<EOF
+
+config wifi-device 'radio0'
+	option type 'mac80211'
+	option path 'platform/soc@0/c000000.wifi'
+	option channel '36'
+	option band '5g'
+	option htmode 'HE80'
+	option cell_density '0'
+
+config wifi-iface 'default_radio0'
+	option device 'radio0'
+	option network 'lan'
+	option mode 'ap'
+	option ssid 'jdc-5g'
+	option encryption 'sae-mixed'
+	option key '1234567890'
+
+config wifi-device 'radio1'
+	option type 'mac80211'
+	option path 'platform/soc@0/c000000.wifi+1'
+	option channel '1'
+	option band '2g'
+	option htmode 'HE20'
+	option disabled '1'
+
+config wifi-iface 'default_radio1'
+	option device 'radio1'
+	option network 'lan'
+	option mode 'ap'
+	option ssid 'OpenWrt'
+	option encryption 'none'
+
+EOF
+
+
